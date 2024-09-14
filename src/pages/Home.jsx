657f4f2d8b4312c461c9ded61c4533720887f4a1
Robinson <robsonkhzepflo@gmail.com>
2024-09-14 23:55:19 +0530

    footer integrated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Topbar from '../components/Topbar'
 import MainPage from '../components/MainPage'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
     <div>
         <Topbar />
         <MainPage/>
+        <Footer />
     </div>
   )
 }
