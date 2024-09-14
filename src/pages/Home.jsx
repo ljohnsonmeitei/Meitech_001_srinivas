6b126ab0d10210344bdb246e725189dc064dff29
Johnson <49550500+ljohnsonmeitei@users.noreply.github.com>
2024-09-15 00:33:34 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Topbar from '../components/Topbar'
 import MainPage from '../components/MainPage'
+import Carousel from '../components/carousel/carousel'
 import Footer from '../components/Footer'
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <div>
         <Topbar />
         <MainPage/>
+        <Carousel />
         <Footer />
     </div>
   )
