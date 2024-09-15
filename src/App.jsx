979a2e04e109238e09bf986af6f34361395b1ae7
Ellison07 <118790615+Ellison07@users.noreply.github.com>
2024-09-15 08:37:37 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import SignIn from './components/SignIn/SignIn'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<About />} />
+        <Route path="/signIn" element = {<SignIn/>}/>
       </Routes>
     </BrowserRouter>
   )
